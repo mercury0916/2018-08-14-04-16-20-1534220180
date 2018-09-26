07b72376fee6f556f72f6918655a91f262e647c9
余茜 <yuqian@yuqiandeMacBook-Pro.local>
2018-09-27 00:48:51 +0800

    find minium

--- main/main.js
+++ main/main.js
@@ -1,18 +1,37 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
+o)最大值 = ${sequence.maxium()}
+o)元素数量 = ${sequence.number()}
+o)平均值 = ${sequence.average()}
 `);
 };
 
 class Sequence {
   constructor(input) {
-    // Write your code here
+  sequence = input;  
   }
 
   minimum() {
-    // Write your code here
+    sequence.sort();
+    return sequence[0];
+  }
+  
+  maxium(){
+    sequence.sort();
+    return sequence[sequence.length - 1];
+  }
+ 
+  number(){
+    return arguments.length;
+
   }
 
-  // Write your code here
-}
+ average(){
+   var sum = 0;
+   for(var i of sequence){
+      sum += sum; 
+    }
+   return sum / arguments.length ;
+   }
+} 
